c60132f5a60c75bd0345feff8396cd3c9bbda443
Nikolay Voronov <nkvoronov@gmail.com>
2022-06-12 22:57:02 +0000

    Update to version 10.0.0 (028)

--- service.jellyfin/web/dashboard-notifications-notifications-index-html.aa1a4260a72a65320b1c.bundle.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[8399],{41085:function(i){i.exports='<div id="notificationSettingsPage" data-role="page" class="page type-interior notificationConfigurationPage"> <div> <div class="content-primary"> <div class="readOnlyContent"> <div class="notificationList"></div> </div> </div> </div> </div> '}}]);
